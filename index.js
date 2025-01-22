5b26e12db1dc5ac0472c2f7d019428ec7df744d3
nguyenminhduc1982005@gmail.com <nguyenminhduc1982005@gmail.com>
2025-01-22 20:45:14 +0700

    Fix: change image url

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const products = require('./db');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const products = require("./db");
 
 const app = express();
 const PORT = 4000;
@@ -11,16 +11,26 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 function getImages(productName) {
   return Array(5)
     .fill(0)
-    .map((name, index) => `http://localhost:${PORT}/images/${productName}-${index + 1}.jpg`);
+    .map(
+      (name, index) =>
+        `https://furniro-api-j4bw.onrender.com/images/${productName}-${
+          index + 1
+        }.jpg`
+    );
 }
+// function getImages(productName) {
+//   return Array(5)
+//     .fill(0)
+//     .map((name, index) => `http://localhost:${PORT}/images/${productName}-${index + 1}.jpg`);
+// }
 
 // API GET /product
-app.get('/product', (req, res) => {
+app.get("/product", (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 10;
   const startIndex = (page - 1) * limit;
@@ -30,24 +40,29 @@ app.get('/product', (req, res) => {
     currentPage: page,
     totalItems: products.length,
     totalPages: Math.ceil(products.length / limit),
-    data: products.map(item => ({ ...item, images: getImages(item.name.split(' ')[0].toLocaleLowerCase()) })).slice(startIndex, endIndex),
+    data: products
+      .map((item) => ({
+        ...item,
+        images: getImages(item.name.split(" ")[0].toLocaleLowerCase()),
+      }))
+      .slice(startIndex, endIndex),
   };
 
   res.json(result);
 });
 
 // API GET /product/:id
-app.get('/product/:id', (req, res) => {
+app.get("/product/:id", (req, res) => {
   const productId = parseInt(req.params.id);
-  const product = products.find(p => p.id === productId);
+  const product = products.find((p) => p.id === productId);
 
   if (!product) {
-    return res.status(404).json({ message: 'Product not found' });
+    return res.status(404).json({ message: "Product not found" });
   }
 
   res.json({
     ...product,
-    images: getImages(product.name.split(' ')[0].toLocaleLowerCase()),
+    images: getImages(product.name.split(" ")[0].toLocaleLowerCase()),
   });
 });
 
